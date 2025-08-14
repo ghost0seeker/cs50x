a74aa1119fb74ebacee77cc3f32f286c92853802
piyush <piyushkumar16@protonmail.com>
2025-08-14 15:30:09 +0530

    implemented string linear search by using sum of chars

--- lecture-3-algorithms/search.c
+++ lecture-3-algorithms/search.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+
+int strsum(string s);
 
 int main(void)
 {
-    int numbers[7];
-    int n = get_int("Number: ");
+    string strings[] = {"battleship", "boat", "cannon", "iron", "thimble", "top hat"};
 
-    // linear search
-    for (int i = 0; i < 7; i++)
+    string s = get_string("String: ");
+
+    int search_string_Sum = strsum(s);
+
+    for (int i = 0; i < 6; i++)
     {
-        if (numbers[i] == n)
+        int array_string_sum = strsum(strings[i]);
+        if (array_string_sum == search_string_Sum)
         {
-            printf("Found %i at %i", n, i);
+            printf("Found\n");
             return 0;
         }
     }
-
-    printf("%i not found", n);
+    printf("Not found\n");
     return 1;
+}
+
+int strsum(string s)
+{
+    int ss = 0;
+
+    for (int i = 0, l = strlen(s); i < l; i++)
+    {
+        ss += s[i];
+    }
+
+    return ss;
 }
